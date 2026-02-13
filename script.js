2ee5cee88aea97d4279dc66d6ccc350d076ce113
Mohammadreza Hassani <252051910+hassanimohammadreza@users.noreply.github.com>
2026-02-13 19:08:21 +0330

    Update script.js

--- script.js
+++ script.js
@@ -4,23 +4,19 @@ document.addEventListener("DOMContentLoaded", function () {
   galleryItems.forEach(item => {
     const img = item.querySelector('img');
     
-    // Make sure the image is loaded before calculating its dimensions
     img.onload = function () {
       const aspectRatio = img.naturalWidth / img.naturalHeight;
       const itemWidth = item.offsetWidth;
       const itemHeight = itemWidth / aspectRatio;
 
-      // Adjust the height of the gallery item
-      item.style.height = `${itemHeight + 50}px`; // Add 50px for padding and text
+      item.style.height = `${itemHeight + 50}px`;
     }
 
-    // If the image is already loaded
     if (img.complete) {
       img.onload();
     }
   });
 
-  // Ensure the footer sticks to the bottom if the content is small
   const footer = document.querySelector('footer');
   if (document.body.scrollHeight <= window.innerHeight) {
     footer.style.position = 'absolute';
@@ -28,3 +24,4 @@ document.addEventListener("DOMContentLoaded", function () {
     footer.style.width = '100%';
   }
 });
+
